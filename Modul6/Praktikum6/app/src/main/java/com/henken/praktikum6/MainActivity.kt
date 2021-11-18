7b7406151b042a9c30d72a86ef951ff4eda2b399
yoihenken <18104005@ittelkom-pwt.ac.id>
2021-11-18 16:50:38 +0700

    Update MainActivity.kt

--- Modul6/Praktikum6/app/src/main/java/com/henken/praktikum6/MainActivity.kt
+++ Modul6/Praktikum6/app/src/main/java/com/henken/praktikum6/MainActivity.kt
@@ -2,10 +2,46 @@ package com.henken.praktikum6
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.henken.praktikum6.databinding.ActivityMainBinding
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private val list = ArrayList<MyData>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        list.addAll(getListMyDatas())
+        showRecyclerList()
     }
+
+    fun getListMyDatas(): ArrayList<MyData> {
+        val dataName = resources.getStringArray(R.array.data_name)
+        val dataDescription = resources.getStringArray(R.array.data_description)
+        val dataPhoto = resources.getStringArray(R.array.data_photo)
+        val listMyData = ArrayList<MyData>()
+        for (position in dataName.indices) {
+            val myData = MyData(
+                dataName[position],
+                dataDescription[position],
+                dataPhoto[position]
+            )
+            listMyData.add(myData)
+        }
+        return listMyData
+    }
+
+    private fun showRecyclerList() {
+        binding.apply {
+            rv_pariwisata.layoutManager = LinearLayoutManager(this@MainActivity)
+            val listMyDataAdapter = MainAdapter(list)
+            rv_pariwisata.adapter = listMyDataAdapter
+        }
+    }
+
 }
